f558d5cafa30b927ad97d1e5fb9b902e6c270573
minjaeeu <minjaeeu@outlook.com>
2024-04-20 20:15:27 -0400

    script to initiate uvicorn

---
+++ main.py
@@ -0,0 +1,15 @@
+from server_config import host, port
+import subprocess
+
+# invoking uvicorn to run our server app at server_config.host and server_config.port
+command = f"uvicorn server:app --host {host} --port {port} --reload"
+
+# initiating process
+process = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE)
+
+# capturing process error
+error = process.communicate()
+
+# print errors if any
+if error:
+    print("Error:", error.decode("utf-8"))
---
+++ server_config.py
@@ -0,0 +1,5 @@
+host = "localhost"
+"""host address for the server"""
+
+port = 80
+"""port address for the server"""
